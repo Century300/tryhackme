5a6901fb8768ae3b2dc21ed23daed543fd16cd9e
Century300 <67885281+Century300@users.noreply.github.com>
2021-11-28 10:55:56 -0600

    Update install_part2.sh

--- install_part2.sh
+++ install_part2.sh
@@ -23,8 +23,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # Fix permission
 chmod 700 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Install autojump to use the plugin autojump later
-sudo apt install autojump -y
+# Debian dependencies to use the pluggins later
+sudo apt install -y zsh-syntax-highlighting autojump zsh-autosuggestions
 
 # Clone the powerlevel10k repository to oh-my-zsh directory
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
